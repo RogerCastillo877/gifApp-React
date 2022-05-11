868fcccfe851fb13197935f46c3bb6314fc2d45d
RogerCastillo877 <roger.castillo877@gmail.com>
2022-05-11 17:51:16 -0500

    Add CSS and create a helper

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
@@ -6,40 +7,25 @@ export const GifGrid = ({ category }) => {
   const [ images, setImages ] = useState([]);
 
   useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async() => {
-
-    const url = `https://api.giphy.com/v1/gifs/search?q=dragon&limit=10&api_key=TCW03PyjHaaw6LKmcpI1UvjmLEpCKKns`
-    const resp = await fetch( url );
-    const { data } = await resp.json();
-
-    const gifs = data.map( img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url
-      } 
-    })
-
-    console.log(gifs);
-    setImages( gifs );
-  };
+    getGifs( category )
+      .then( img => setImages( img ) );
+  }, [ category ]);
 
   return (
     <>
       <h3>{ category }</h3>
-      <ol>
-        {
-          images.map( (img) => ( 
-            <GifGridItem
-              key={ img.id }
-              { ...img }
-            />
-          ))
-        }
-      </ol>
+      <div className='card-grid'>
+        <ol>
+          {
+            images.map( (img) => ( 
+              <GifGridItem
+                key={ img.id }
+                { ...img }
+              />
+            ))
+          }
+        </ol>
+      </div>
     </>
   )
 }
