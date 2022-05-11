411976514d96c9a86b748fff2e10677cf1e3865b
RogerCastillo877 <roger.castillo877@gmail.com>
2022-05-10 22:49:28 -0500

    Show title and images of search

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
+  const [ images, setImages ] = useState([]);
+
+  useEffect(() => {
+    getGifs();
+  }, []);
+
   const getGifs = async() => {
 
-    // const url = `https://api.giphy.com/v1/gifs/search?=${'dragon'}&limit=${10}&api_key=${'TCW03PyjHaaw6LKmcpI1UvjmLEpCKKns'}`
     const url = `https://api.giphy.com/v1/gifs/search?q=dragon&limit=10&api_key=TCW03PyjHaaw6LKmcpI1UvjmLEpCKKns`
-    //TCW03PyjHaaw6LKmcpI1UvjmLEpCKKns
     const resp = await fetch( url );
     const { data } = await resp.json();
 
@@ -17,14 +22,24 @@ export const GifGrid = ({ category }) => {
         url: img.images?.downsized_medium.url
       } 
     })
-    console.log(gifs);
-  };
 
-  getGifs();
+    console.log(gifs);
+    setImages( gifs );
+  };
 
   return (
     <>
       <h3>{ category }</h3>
+      <ol>
+        {
+          images.map( (img) => ( 
+            <GifGridItem
+              key={ img.id }
+              { ...img }
+            />
+          ))
+        }
+      </ol>
     </>
   )
 }
---
+++ src/components/GifGridItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const GifGridItem = ({ title, url }) => {
+  
+  return (
+    <div>
+      <img
+        src={ url }
+        alt={ title }
+      />
+      <p>{ title }</p>
+    </div>
+  )
+}
